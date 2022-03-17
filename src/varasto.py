231b2a15c5cf36f7851eeb1476081d82a53b2de7
Marianna Ojanen <marianna.ojanen@kotiposti.net>
2022-03-17 23:33:39 +0200

    Added pylint

--- src/varasto.py
+++ src/varasto.py
@@ -5,11 +5,10 @@ class Varasto:
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-        
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
+
+        alku_saldo = max(alku_saldo, 0)
+
+        if alku_saldo <= tilavuus:
             # mahtuu
             self.saldo = alku_saldo
         else:
@@ -31,7 +30,7 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -40,6 +39,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
